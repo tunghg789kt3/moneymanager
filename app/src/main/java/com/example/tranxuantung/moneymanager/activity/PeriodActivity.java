67d6ee19a4d411ed6eec5c35dc6b38c3175d721e
tunghg789kt3 <trantunghg1998@gmail.com>
2018-03-28 07:34:14 +0700

    More update

--- app/src/main/java/com/example/tranxuantung/moneymanager/activity/PeriodActivity.java
+++ app/src/main/java/com/example/tranxuantung/moneymanager/activity/PeriodActivity.java
@@ -1,11 +1,14 @@
 package com.example.tranxuantung.moneymanager.activity;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.example.tranxuantung.moneymanager.R;
@@ -14,12 +17,20 @@ import com.example.tranxuantung.moneymanager.adapter.ChukiPagerAdapter;
 public class PeriodActivity extends AppCompatActivity {
     private ViewPager pager;
     private TabLayout tabLayout;
-
+    private FloatingActionButton addthuchi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_period);
         addControl();
+        addthuchi = (FloatingActionButton) findViewById(R.id.floattingButton);
+        addthuchi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PeriodActivity.this, AddThuChiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     private void addControl(){
         pager = (ViewPager) findViewById(R.id.viewPager);
