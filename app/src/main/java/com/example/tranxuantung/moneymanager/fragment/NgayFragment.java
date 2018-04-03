7095026ddc870001803aee2be5d36193d35bad6a
tunghg789kt3 <trantunghg1998@gmail.com>
2018-04-03 10:12:05 +0700

    More update

--- app/src/main/java/com/example/tranxuantung/moneymanager/fragment/NgayFragment.java
+++ app/src/main/java/com/example/tranxuantung/moneymanager/fragment/NgayFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.tranxuantung.moneymanager.R;
 
@@ -20,6 +19,6 @@ public class NgayFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ngay, container, false);
+        return inflater.inflate(R.layout.fragment_tuan, container, false);
     }
 }
