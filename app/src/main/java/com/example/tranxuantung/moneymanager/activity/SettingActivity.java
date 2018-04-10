6ad9ab5d05b9d66e022447a5f232aef587dca263
tunghg789kt3 <trantunghg1998@gmail.com>
2018-04-10 10:16:02 +0700

    shit

--- app/src/main/java/com/example/tranxuantung/moneymanager/activity/SettingActivity.java
+++ app/src/main/java/com/example/tranxuantung/moneymanager/activity/SettingActivity.java
@@ -2,14 +2,30 @@ package com.example.tranxuantung.moneymanager.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.tranxuantung.moneymanager.R;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener{
+    private LinearLayout lnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        lnSetting = findViewById(R.id.lnSetting);
+        lnSetting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.lnSetting:
+
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/tranxuantung/moneymanager/fragment/ThuFragment.java
+++ app/src/main/java/com/example/tranxuantung/moneymanager/fragment/ThuFragment.java
@@ -18,8 +18,8 @@ import com.example.tranxuantung.moneymanager.database.Data;
 import java.text.SimpleDateFormat;
 
 public class ThuFragment extends Fragment {
-    EditText edtNgayThu, edtTaikhoanThu, edtDanhmucThu, edtSotienThu, edtDoituongThu, ghiChuThu;
-    Button btnAddThu, btnErase;
+    EditText edtNgayThu, edtTaikhoanThu, edtDanhmucThu, edtSotienThu, edtDoituongThu, edtGhichuThu;
+    Button btnAddThu, btnEraseThu;
     Data data;
     long ngayHT;
 
@@ -29,19 +29,19 @@ public class ThuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_thu, container, false);
-        edtNgayThu = (EditText)view.findViewById(R.id.edtNgayThu);
-        edtTaikhoanThu = (EditText)view.findViewById(R.id.edtTaikhoanThu);
-        edtDanhmucThu = (EditText)view.findViewById(R.id.edtDanhmucThu);
-        edtSotienThu = (EditText)view.findViewById(R.id.edtSotienThu);
-        edtDoituongThu = (EditText)view.findViewById(R.id.edtDoituongThu);
-        ghiChuThu = (EditText)view.findViewById(R.id.ghiChuThu);
+        edtNgayThu = (EditText) view.findViewById(R.id.edtNgayThu);
+        edtTaikhoanThu = (EditText) view.findViewById(R.id.edtTaikhoanThu);
+        edtDanhmucThu = (EditText) view.findViewById(R.id.edtDanhmucThu);
+        edtSotienThu = (EditText) view.findViewById(R.id.edtSotienThu);
+        edtDoituongThu = (EditText) view.findViewById(R.id.edtDoituongThu);
+        edtGhichuThu = (EditText) view.findViewById(R.id.edtGhichuThu);
         ngayHT = System.currentTimeMillis();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 
         edtNgayThu.setText(formatter.format(ngayHT));
 
-        btnAddThu = (Button)view.findViewById(R.id.btnAddThu);
-        btnErase = (Button)view.findViewById(R.id.btnErase);
+        btnAddThu = (Button) view.findViewById(R.id.btnAddThu);
+        btnEraseThu = (Button) view.findViewById(R.id.btnEraseThu);
 
         data = new Data(getContext());
 
@@ -52,12 +52,37 @@ public class ThuFragment extends Fragment {
                 String danhmucThu = edtDanhmucThu.getText().toString().trim();
                 double sotienThu = Double.parseDouble(edtTaikhoanThu.getText().toString().trim());
                 String doituongThu = edtDoituongThu.getText().toString().trim();
-                String ghichuThu = ghiChuThu.getText().toString().trim();
+                String ghichuThu = edtGhichuThu.getText().toString().trim();
                 DTTaiKhoan dtTaiKhoan = new DTTaiKhoan(ngayHT, taikhoanThu, danhmucThu, sotienThu, doituongThu, ghichuThu);
 
-                if (data.saveThu(dtTaiKhoan)){
-                    Toast.makeText(getContext(), "Đã lưu thành công!", Toast.LENGTH_SHORT).show();
-                }else Toast.makeText(getContext(), "Lỗi!", Toast.LENGTH_SHORT).show();
+                if (taikhoanThu == "") {
+                    Toast.makeText(getContext(), "Tài khoản không được để trống!", Toast.LENGTH_SHORT).show();
+                    edtTaikhoanThu.requestFocus();
+                }else if (danhmucThu == ""){
+                    Toast.makeText(getContext(), "Danh mục không được để trống!", Toast.LENGTH_SHORT).show();
+                    edtDanhmucThu.requestFocus();
+                }else if (edtSotienThu.getText().toString().trim() == ""){
+                    Toast.makeText(getContext(), "Số tiền không được để trống!", Toast.LENGTH_SHORT).show();
+                    edtSotienThu.requestFocus();
+                }else if (doituongThu == ""){
+                    Toast.makeText(getContext(), "Đối tượng không được để trống!", Toast.LENGTH_SHORT).show();
+                    edtDoituongThu.requestFocus();
+                }else{
+                    if (data.saveThu(dtTaiKhoan)) {
+                        Toast.makeText(getContext(), "Đã lưu thành công!", Toast.LENGTH_SHORT).show();
+                    } else Toast.makeText(getContext(), "Lỗi!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnEraseThu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtTaikhoanThu.setText("");
+                edtDanhmucThu.setText("");
+                edtSotienThu.setText("");
+                edtDoituongThu.setText("");
+                edtGhichuThu.setText("");
             }
         });
 
